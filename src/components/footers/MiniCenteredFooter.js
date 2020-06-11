69b6b7dac51245213de2f645f5cca0166dde28b1
leonjaques <leonardomjq@gmail.com>
2020-06-11 01:21:40 +0100

    fixed footer

--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -5,6 +5,7 @@ import { Container as ContainerBase } from "components/misc/Layouts.js";
 import logo from "../../images/logo-mt.png";
 import { ReactComponent as FacebookIcon } from "../../images/facebook-icon.svg";
 import { ReactComponent as LinkedInIcon } from "../../images/linkedin-icon.svg";
+import { Link } from "react-router-dom";
 
 const Container = tw(ContainerBase)`bg-gray-900 text-gray-100 -mx-8 -mb-8`;
 const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
@@ -16,7 +17,7 @@ const LogoImg = tw.img`w-8`;
 const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
 
 const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
-const Link = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
+const NavLink = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
 
 const SocialLinksContainer = tw.div`mt-10`;
 const SocialLink = styled.a`
@@ -37,10 +38,18 @@ export default () => {
             <LogoText>Instituto Methodus</LogoText>
           </LogoContainer>
           <LinksContainer>
-            <Link href="/">Home</Link>
-            <Link href="/sobre">Sobre</Link>
-            <Link href="/clientes">Clientes</Link>
-            <Link href="/faleconosco">Fale Conosco</Link>
+            <Link exact to="/">
+              <NavLink>Home</NavLink>
+            </Link>
+            <Link exact to="/sobre">
+              <NavLink>Sobre</NavLink>
+            </Link>
+            <Link exact to="/clientes">
+              <NavLink>Clientes</NavLink>
+            </Link>
+            <Link exact to="/faleconosco">
+              <NavLink>Fale Conosco</NavLink>
+            </Link>
           </LinksContainer>
           <SocialLinksContainer>
             <SocialLink
